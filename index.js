b1757f398036672c2edc16eb775b1053d9b7164e
Ricardo do Vale <ricardovcza@gmail.com>
2022-09-06 16:12:48 -0300

    Substituído .innerHtml

--- index.js
+++ index.js
@@ -28,45 +28,82 @@ const productsCart = [
 
 let soma = 0
 const body = document.querySelector('body')
+//main
 const main = document.createElement('main')
 main.classList = "lista container"
-main.innerHTML = `
-    <h1>Virtual Market</h1>    
-    <header class="cabecalho">
-        <span>Item</span>
-        <span>Valor</span>            
-    </header>
-    <ul class="productList">    
-    </ul>
-    <section class="rodape">
-    <div class="total">
-        <span>Total</span>        
-    </div>
-    <button class="buttonEnd">Finalizar Compra</button>
-    </section>
-`
+//h1
+const h1 = document.createElement('h1')
+h1.innerText = 'Virtual Market'
+main.appendChild(h1)
+//cabecalho
+const header = document.createElement('header')
+header.classList = "cabecalho"
+const itemCabecalho = document.createElement('span')
+const valorCabecalho = document.createElement('span')
+itemCabecalho.innerText = 'Item'
+valorCabecalho.innerText = 'Valor'
+header.append(itemCabecalho, valorCabecalho)
+main.appendChild(header)
+
+//UL
+const ul = document.createElement('ul')
+ul.classList = 'productList'
+main.append(ul)
+inserirItens(productsCart)
+
+//section
+const section = document.createElement('section')
+section.classList = 'rodape'
+
+const divSection = document.createElement('div')
+divSection.classList = 'total'
+
+const spanTotal = document.createElement('span')
+const spanValorTotal = document.createElement('span')
+spanTotal.innerText = 'Total'
+spanValorTotal.innerText = somarValores(productsCart)
+
+const button = document.createElement('button')
+button.classList = 'buttonEnd'
+button.innerText = 'Finalizar Compra'
+
+divSection.append(spanTotal, spanValorTotal)
+section.append(divSection, button)
+
+main.append(section)
 body.append(main)
 
-const productList = document.querySelector('.productList')
 
-for(let i in productsCart) {
-    let produtoAtual = productsCart[i]
-    let nomeProduto = produtoAtual.name
-    let precoProduto = produtoAtual.price
-    soma += precoProduto                //FUNÇÃO SOMA
-    precoProduto = String(precoProduto)    
-    const li = document.createElement('li')
-    li.classList = "productDetails"
-    li.innerHTML = `
-        <span>${nomeProduto}</span>
-        <span>${precoProduto.replace(".", ",")}
-    `
-    productList.appendChild(li)
-    
+function inserirItens(arrObj) {
+        
+    for(let i in arrObj) {
+        let produtoAtual = productsCart[i]
+        let nomeProduto = produtoAtual.name
+        let precoProduto = produtoAtual.price
+                
+        const li = document.createElement('li')
+        li.classList = "productDetails"
+       
+        const spanNome = document.createElement('span')
+        const spanPreco = document.createElement('span')
+        spanNome.innerText = nomeProduto
+        spanPreco.innerText = convertePontoVirgula(precoProduto)               
+
+        li.append(spanNome, spanPreco)
+        ul.append(li)
+    }    
 }
 
-const total = document.querySelector('.total')
-const spanTotal = document.createElement('span')
-soma = String(soma)
-spanTotal.innerHTML = `R$ ${soma.replace(".", ",")}`
-total.appendChild(spanTotal)
+function convertePontoVirgula(valor) {
+    let numeroString = String(valor)
+    return numeroString.replace('.',',')
+}
+
+function somarValores(arr) {
+    for(let i in arr) {
+        let precoAtual = arr[i].price
+        soma += precoAtual
+    }
+    return convertePontoVirgula(soma)
+
+}
